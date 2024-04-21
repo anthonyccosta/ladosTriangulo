4bca9bc8124bf50da5fa22346290aca33a6a084f
anthonyccosta <ccosta.anthony@gmail.com>
2024-04-21 15:01:52 -0300

    primeiro commit

---
+++ ladosTriangulo/Program.cs
@@ -0,0 +1,24 @@
+﻿double ladoA, ladoB, ladoC;
+
+Console.Write("Digite o valor do lado A: ");
+ladoA = double.Parse(Console.ReadLine());
+
+Console.Write("Digite o valor do lado B: ");
+ladoB = double.Parse(Console.ReadLine());
+
+Console.Write("Digite o valor do lado C: ");
+ladoC = double.Parse(Console.ReadLine());
+
+// verificação do tipo de triângulo
+if (ladoA == ladoB && ladoB == ladoC) // triângulo equilátero
+    {
+        Console.WriteLine("O triângulo é equilátero.");
+    }
+else if (ladoA == ladoB || ladoA == ladoC || ladoB == ladoC) // triângulo isósceles
+    {
+        Console.WriteLine("O triângulo é isósceles.");
+    }
+else // triângulo escaleno
+    {
+        Console.WriteLine("O triângulo é escaleno.");
+    }
